859a66ae7a2ab77f2a8de319fa1566bbdc48f53d
Umran Abdo <Umranabdo112@gmail.com>
2025-02-26 16:49:15 +0200

    Fix Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/', // تأكد من إضافة هذا السطر
   plugins: [react()],
+  base: './', // جرّب وضع "./" بدلاً من "/"
 })
